03952e1c1e02f079e072b8fc22a108f618fd858a
Sachin <sachin_kosare@yahoo.com>
2025-03-21 15:01:17 +0530

    Initial setup

---
+++ js/templatemo_script.js
@@ -0,0 +1,112 @@
+// hack Google Maps to bypass API v3 key (needed since 22 June 2016 http://googlegeodevelopers.blogspot.com.es/2016/06/building-for-scale-updates-to-google.html)
+var target = document.head;
+var observer = new MutationObserver(function(mutations) {
+    for (var i = 0; mutations[i]; ++i) { // notify when script to hack is added in HTML head
+        if (mutations[i].addedNodes[0].nodeName == "SCRIPT" && mutations[i].addedNodes[0].src.match(/\/AuthenticationService.Authenticate?/g)) {
+            var str = mutations[i].addedNodes[0].src.match(/[?&]callback=.*[&$]/g);
+            if (str) {
+                if (str[0][str[0].length - 1] == '&') {
+                    str = str[0].substring(10, str[0].length - 1);
+                } else {
+                    str = str[0].substring(10);
+                }
+                var split = str.split(".");
+                var object = split[0];
+                var method = split[1];
+                window[object][method] = null; // remove censorship message function _xdc_._jmzdv6 (AJAX callback name "_jmzdv6" differs depending on URL)
+                //window[object] = {}; // when we removed the complete object _xdc_, Google Maps tiles did not load when we moved the map with the mouse (no problem with OpenStreetMap)
+            }
+            observer.disconnect();
+        }
+    }
+});
+var config = { attributes: true, childList: true, characterData: true }
+observer.observe(target, config);
+
+var top_menu_height = 0;
+jQuery(function($) {
+		$(window).load( function() {
+			$('.external-link').unbind('click');	
+		});
+		
+        $(document).ready( function() {
+
+            // load google map
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'http://maps.google.com/maps/api/js?sensor=false&' +
+            'callback=initialize';
+        document.body.appendChild(script);
+
+        top_menu_height = $('.templatemo-top-menu').height();
+        // scroll spy to auto active the nav item
+        $('body').scrollspy({ target: '#templatemo-nav-bar', offset: top_menu_height + 10 });
+		$('.external-link').unbind('click');
+
+        // scroll to top
+        $('#btn-back-to-top').click(function(e){
+            e.preventDefault();
+            scrollTo('#templatemo-top');
+        });
+
+        // scroll to specific id when click on menu
+        $('.templatemo-top-menu .navbar-nav a').click(function(e){
+            e.preventDefault(); 
+            var linkId = $(this).attr('href');
+            scrollTo(linkId);
+            if($('.navbar-toggle').is(":visible") == true){
+                $('.navbar-collapse').collapse('toggle');
+            }
+            $(this).blur();
+            return false;
+        });
+		/*
+		var dt = window.atob('IHwgRGVzaWduOiA8YSByZWw9Im5vZm9sbG93IiBocmVmPSJodHRwOi8vd3d3LnRlbXBsYXRlbW8uY29tL3RtLTM5NS11cmJhbmljIiB0YXJnZXQ9Il9wYXJlbnQiPlVyYmFuaWM8L2E+'); // decode the string
+		var div = document.getElementById('footer-line');
+		div.innerHTML = div.innerHTML + dt;
+		*/
+        // to stick navbar on top
+        $('.templatemo-top-menu ').stickUp();
+
+        // gallery category
+        $('.templatemo-gallery-category a').click(function(e){
+            e.preventDefault(); 
+            $(this).parent().children('a').removeClass('active');
+            $(this).addClass('active');
+            var linkClass = $(this).attr('href');
+            $('.gallery').each(function(){
+                if($(this).is(":visible") == true){
+                   $(this).hide();
+                };
+            });
+            $(linkClass).fadeIn();  
+        });
+
+        //gallery light box setup
+        $('a.colorbox').colorbox({
+                                    rel: function(){
+                                        return $(this).data('group');
+
+                                    }
+        });
+    });
+});
+
+function initialize() {
+    var mapOptions = {
+      zoom: 12,
+      center: new google.maps.LatLng(24.9714954,55.1559399)
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+}
+
+// scroll animation 
+function scrollTo(selectors)
+{
+
+    if(!$(selectors).size()) return;
+    var selector_top = $(selectors).offset().top - top_menu_height;
+    $('html,body').animate({ scrollTop: selector_top }, 'slow');
+
+}
